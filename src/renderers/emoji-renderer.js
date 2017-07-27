46cdf96ea4adf02efabe83e81ea0de8153d9df3c
Jared Lunde <jared.lunde@gmail.com>
2017-07-27 12:03:01 -0500

    Improve accessibility for users with disabilities
    Added an aria role and label to the emoji <span>

--- src/renderers/emoji-renderer.js
+++ src/renderers/emoji-renderer.js
@@ -2,21 +2,28 @@ import React from 'react';
 import {sprite} from '../styles/emojione-sprite';
 import {codepointToShort, codepointToUnicode} from '../utils/emoji-format-conversion';
 
-const Emoji = ({codepoint, style = {}, onClick}) => (
-    <span
-        onClick={onClick}
-        style={sprite(codepoint, style)}
-        title={codepointToShort.get(codepoint)}
-    >
-        {codepointToUnicode.get(codepoint)}
-    </span>
-);
+const Emoji = ({codepoint, style = {}, onClick, ariaLabel}) => {
+    const shortCodepoint = codepointToShort.get(codepoint);
+    
+    return (
+        <span
+            onClick={onClick}
+            style={sprite(codepoint, style)}
+            title={shortCodepoint}
+            role='img'
+            aria-label={ariaLabel || shortCodepoint}
+        >
+            {codepointToUnicode.get(codepoint)}
+        </span>  
+    );
+};
 
-const getRenderer = ({style, onClick}) => (codepoint, key) => (
+const getRenderer = ({style, onClick, ariaLabel, ...props}) => (codepoint, key) => (
     <Emoji
         codepoint={codepoint}
         style={style}
         onClick={onClick}
+        ariaLabel={props['aria-label']}
         key={key}
     />
 );
